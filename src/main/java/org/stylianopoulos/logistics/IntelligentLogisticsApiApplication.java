c4f2ca38c718da0a2f005eaa1ed28974e2ce8920
george <gstylianop@gmail.com>
2026-01-04 00:41:57 +0200

    Added PostgreSQL as Docker

---
+++ src/main/java/org/stylianopoulos/logistics/IntelligentLogisticsApiApplication.java
@@ -0,0 +1,13 @@
+package org.stylianopoulos.logistics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntelligentLogisticsApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntelligentLogisticsApiApplication.class, args);
+    }
+
+}
---
+++ src/main/java/org/stylianopoulos/logistics/controller/HomeController.java
@@ -0,0 +1,27 @@
+package org.stylianopoulos.logistics.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import java.util.List;
+
+// * INFO: Senior Implementation: Handles both UI and REST endpoints
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String showIndex() {
+        return "index";
+    }
+
+    // ? MUST include @ResponseBody if return a List from a @Controller
+    @GetMapping("/api/v1/health")
+    @ResponseBody
+    public List<String> getHealth() {
+        return List.of("Kernel", "Network", "Storage")
+                .stream()
+                .map(String::toUpperCase)
+                .map(s -> s + ": ACTIVE")
+                .toList();
+    }
+}
