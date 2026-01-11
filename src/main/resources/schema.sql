fbd3b835cec89e30a73aafe00420a03afe378543
george <gstylianop@gmail.com>
2026-01-11 21:36:22 +0200

    changed to a more suitable schema

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,11 +1,18 @@
 DROP TABLE IF EXISTS shipping_requests;
 
 CREATE TABLE shipping_requests (
-   id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-   vehicle_type VARCHAR(50) NOT NULL,
-   strategy_type VARCHAR(50) NOT NULL,
-   destination VARCHAR(255) NOT NULL,
+   id SERIAL PRIMARY KEY,   vehicle_type VARCHAR(50) NOT NULL,
    payload_weight DOUBLE PRECISION NOT NULL,
-   status VARCHAR(20) NOT NULL,
+   velocity DOUBLE PRECISION NOT NULL,
    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+
+DROP TABLE IF EXISTS vehicles;
+
+CREATE TABLE  vehicles (
+    id SERIAL PRIMARY KEY,    vehicle_type VARCHAR(50) NOT NULL,
+    payload_weight DOUBLE PRECISION NOT NULL,
+    velocity DOUBLE PRECISION NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
