4bedfc38627eb74a938ba2f04d55589e6e3c9745
george <gstylianop@gmail.com>
2026-01-15 16:06:15 +0200

    Moved run method to config package

--- src/main/java/org/stylianopoulos/logistics/config/IntelligentLogisticsApiApplication.java
+++ src/main/java/org/stylianopoulos/logistics/config/IntelligentLogisticsApiApplication.java
@@ -1,4 +1,4 @@
-package org.stylianopoulos.logistics;
+package org.stylianopoulos.logistics.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
