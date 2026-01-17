a2b7db938731a4f3a97377e670e559d6467ab3a1
george <gstylianop@gmail.com>
2026-01-17 15:58:43 +0200

    Fixed the error

--- src/main/java/org/stylianopoulos/logistics/service/ShippingContext.java
+++ src/main/java/org/stylianopoulos/logistics/service/ShippingContext.java
@@ -3,6 +3,7 @@ package org.stylianopoulos.logistics.service;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 import org.slf4j.Logger;
@@ -16,25 +17,32 @@ import reactor.core.publisher.Mono;
 public class ShippingContext {
     // ! Using a proper Logger instead of System.out
     private static final Logger log = LoggerFactory.getLogger(ShippingContext.class);
+
     private final Map<String, ShippingStrategy> strategies;
 
     public ShippingContext(List<ShippingStrategy> strategyList) {
         this.strategies = strategyList.stream()
-                .collect(Collectors.toUnmodifiableMap( // ? Unmodifiable map so that the data don't change -> Thread Safety
-                        ShippingStrategy::getStrategyName,
-                        strategy -> strategy
+                .collect(Collectors.<ShippingStrategy, String, ShippingStrategy>toUnmodifiableMap(
+                        strategy -> strategy.getStrategyName().toUpperCase(),
+                        Function.identity()
                 ));
     }
 
+    /*
+       The Map generates a hash-functions from both client's shipping type and
+       from all 3 getShippingName() classes (from ShippingStrategy Interface)
+       and by making the comparison between these 2, it finds what class must
+       use
+     */
     public Mono<Double> execute(String type, double weight) {
         return Mono.justOrEmpty(type)
                 .map(String::toUpperCase)
+                // ? Map to skip if - else
                 .flatMap(key -> Mono.justOrEmpty(strategies.get(key)))
-                // ? The Context does NOT calculate. It calls the strategy's method.
                 .flatMap(strategy -> strategy.calculateCost(weight))
-                // * Log Asynchronously
                 .doOnNext(cost -> log.info("Calculated shipping cost for type {}: {}", type, cost))
                 .switchIfEmpty(Mono.error(() -> new IllegalArgumentException("Unsupported shipping: " + type)));
     }
 }
+
 
