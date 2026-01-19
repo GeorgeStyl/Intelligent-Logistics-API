3007a381921151e8e5f1cb4a1f40aa9a833e760d
george <gstylianop@gmail.com>
2026-01-19 19:07:23 +0200

    No UI is needed

--- src/main/java/org/stylianopoulos/logistics/controller/api/HomeController.java
+++ src/main/java/org/stylianopoulos/logistics/controller/api/HomeController.java
@@ -4,22 +4,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import java.util.List;
+import java.util.stream.Stream;
 
 // * INFO: Senior Implementation: Handles both UI and REST endpoints
 @Controller
 public class HomeController {
 
-    @GetMapping("/")
-    public String showIndex() {
-        return "index";
-    }
-
-    // ? MUST include @ResponseBody if return a List from a @Controller
+    // ? Must include @ResponseBody if return a List from a @Controller
     @GetMapping("/api/v1/health")
     @ResponseBody
     public List<String> getHealth() {
-        return List.of("Kernel", "Network", "Storage")
-                .stream()
+        return Stream.of("Kernel", "Network", "Storage")
                 .map(String::toUpperCase)
                 .map(s -> s + ": ACTIVE")
                 .toList();
