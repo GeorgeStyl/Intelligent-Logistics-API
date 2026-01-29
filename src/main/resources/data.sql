f86f1bf94756cc05b23a317924b04f05e1434da9
george <gstylianop@gmail.com>
2026-01-30 00:34:09 +0200

    Added more data for better statistics

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,10 +1,19 @@
-INSERT INTO vehicles (type, license_plate, capacity, speed, created_at)
-VALUES
-    ('TRUCK', 'GR-TX-9921', 2000, 90, CURRENT_TIMESTAMP),
-    ('VAN', 'GR-VN-4432', 800, 110, CURRENT_TIMESTAMP),
-    ('MOTORCYCLE', 'GR-MC-1122', 50, 120, CURRENT_TIMESTAMP),
-    ('DRONE', 'DRN-001-B', 10, 45, CURRENT_TIMESTAMP),
-    ('TRUCK', 'GR-TX-5566', 2500, 85, CURRENT_TIMESTAMP)
+INSERT INTO vehicles (type, license_plate, capacity, speed, created_at) VALUES
+    ('TRUCK', 'GR-TX-1020', 3000, 85, CURRENT_TIMESTAMP),
+    ('TRUCK', 'GR-TX-4455', 2800, 88, CURRENT_TIMESTAMP),
+    ('TRUCK', 'GR-TX-8899', 3200, 82, CURRENT_TIMESTAMP),
+    ('TRUCK', 'GR-TX-1122', 2900, 90, CURRENT_TIMESTAMP),
+    ('TRUCK', 'GR-TX-3344', 3100, 87, CURRENT_TIMESTAMP),
+    ('VAN', 'GR-VN-2030', 900, 105, CURRENT_TIMESTAMP),
+    ('VAN', 'GR-VN-5566', 850, 115, CURRENT_TIMESTAMP),
+    ('VAN', 'GR-VN-7788', 950, 112, CURRENT_TIMESTAMP),
+    ('VAN', 'GR-VN-9900', 800, 108, CURRENT_TIMESTAMP),
+    ('VAN', 'GR-VN-4433', 880, 110, CURRENT_TIMESTAMP),
+    ('DRONE', 'DRN-003-C', 15, 55, CURRENT_TIMESTAMP),
+    ('DRONE', 'DRN-004-D', 12, 60, CURRENT_TIMESTAMP),
+    ('DRONE', 'DRN-005-E', 20, 50, CURRENT_TIMESTAMP),
+    ('DRONE', 'DRN-006-F', 10, 65, CURRENT_TIMESTAMP),
+    ('DRONE', 'DRN-007-G', 18, 48, CURRENT_TIMESTAMP)
 ON CONFLICT (license_plate) DO NOTHING;
 
 INSERT INTO orders (customer_name, weight, destination, shipping_type, status, cost, created_at) VALUES
@@ -14,3 +23,5 @@ INSERT INTO orders (customer_name, weight, destination, shipping_type, status, c
     ('Delta Manufacturing', 2200.0, 'Heraklion, Greece', 'HEAVY_LOAD', 'PENDING', 4500.00, CURRENT_TIMESTAMP),
     ('Epsilon Tech', 2.5, 'Larissa, Greece', 'EXPRESS', 'IN_PROGRESS', 25.00, CURRENT_TIMESTAMP)
 ON CONFLICT DO NOTHING;
+
+DELETE FROM vehicles WHERE type = 'MOTORCYCLE';
