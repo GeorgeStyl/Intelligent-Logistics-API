1c59586e225c756eee6c53b4d604e59cb552a882
george <gstylianop@gmail.com>
2026-02-01 12:53:05 +0200

    Not needed any more

--- src/main/java/org/stylianopoulos/logistics/service/factory/ErrorFactory.java
+++
@@ -1,9 +0,0 @@
-package org.stylianopoulos.logistics.service.factory;
-
-import org.stylianopoulos.logistics.dto.ErrorResponse;
-import org.springframework.http.HttpStatus;
-
-public interface ErrorFactory {
-    ErrorResponse createResponse(Exception ex);
-    HttpStatus getStatus();
-}
